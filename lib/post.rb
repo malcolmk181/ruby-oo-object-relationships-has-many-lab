ec9fbc351c9b36e30479901da62cb377cebf09bc
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-31 14:33:38 -0400

    commit tree 9e6a7a5d2a7c8c45feeda5a690c875be6110eba3

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(title)
+        @title = title
+        self.class.all << self
+    end
+    
+    def author_name
+        !@author.nil? ? @author.name : nil
+    end
+end
